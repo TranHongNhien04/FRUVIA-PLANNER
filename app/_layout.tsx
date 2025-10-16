a8931d850e7807112d1f03e52f5ede27deb76644
TranHongNhien <hongnhientran.doylely@gmail.com>
2025-10-16 21:11:15 +0700

    feat: Add custom OAuth authentication flow

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,17 @@
-import { Stack } from "expo-router";
+import { Stack } from "expo-router"; 
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import { ClerkProvider, useAuth } from '@clerk/clerk-expo'
 
 export default function RootLayout() {
-  return <Stack />;
+
+
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+    <Stack screenOptions={{
+          headerShown: false
+          }}> 
+      <Stack.Screen name="index"/>
+    </Stack>
+    </ClerkProvider>
+  );
 }
